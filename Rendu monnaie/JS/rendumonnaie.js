48cc8acc135d1001eabb8f4cbbfe23744f56eb17
MatthieuSchoubrenner <mschoubrennerpro@gmail.com>
2021-01-27 14:39:52 +0100

    first commit

---
+++ Rendu monnaie/JS/rendumonnaie.js
@@ -0,0 +1,18 @@
+function test() 
+{
+  var nb1 = document.getElementById('aRegler').nodeValue;
+  var nb2 = document.getElementById('sommeRecu').value;
+  var result;
+
+  result = nb2 - nb1;
+
+  if(result > 0) 
+  {
+    alert('je dois au client '+result+'€');
+  }
+  else 
+  {
+    alert('le client me doit '+(-result)+'€');
+  }
+  console.log(result)
+}
